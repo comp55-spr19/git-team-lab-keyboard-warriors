cc519b371f4d1a63f391bf3fabc8f43eae6e3620
mmendo32 <mmendo32@STK-ECS-2L1JRP2.stk.pacific.edu>
2019-03-19 13:47:47 -0700

    Font is now arial

--- COMP55GitTeamLab/src/SimplePicture.java
+++ COMP55GitTeamLab/src/SimplePicture.java
@@ -1,4 +1,6 @@
 //comment one
+import java.awt.Font;
+
 import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;
@@ -14,6 +16,7 @@ public class SimplePicture extends GraphicsProgram {
 		GImage robot = new GImage("robot.jpg", 200, 100);
 		add(robot);
 		GLabel label = new GLabel("How I feel when using github hackslash", 200, 400);
+		label.setFont("Arial");
 		add(label);
 	}
 }
